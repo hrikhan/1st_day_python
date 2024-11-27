727f7131de449b31fbfa1c469ab1fdb739091c94
hridoykhan51 <hridoykhan6786@gmail.com>
2024-11-28 01:27:06 +0600

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+light =input("current light: ")
+if(light=="green" ):
+    print("go")
+elif(light=="green"):
+    print("stop")
+elif(light==""):
+    print("look at the light")
+else:
+    print("light not working")
+    
